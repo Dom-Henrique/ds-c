6d1d281fa6f8d8aeb8115fb05594ef782520facb
Rene.Soares <rene.silva@academico.ifpb.edu.br>
2026-09-15 11:29:56 -0300

    Add files via upload

---
+++ class-2/Rene-Soares/C.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+
+int linhas, colunas, tam;
+
+void dimensionaMatriz(int l, int c) {
+    linhas = l;
+    colunas = c;
+    tam = l * c;
+}
+
+void zeraMatriz(int vet[]) {
+    for (int i = 0; i < tam; i++) vet[i] = 0;
+}
+
+int indice(int i, int j) {
+    return (i - 1) * colunas + (j - 1);
+}
+
+void adicionaElemento(int vet[], int valor, int i, int j) {
+    vet[indice(i, j)] = valor;
+}
+
+int buscaElemento(int vet[], int i, int j) {
+    return vet[indice(i, j)];
+}
+
+void imprimeMatriz(int vet[], int l, int c) {
+    for (int i = 1; i <= l; i++) {
+        for (int j = 1; j <= c; j++)
+            printf("%4d", vet[indice(i, j)]);
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void somaMatriz() {
+    int a[9] = {1,2,3,4,5,6,7,8,9};
+    int b[9] = {9,8,7,6,5,4,3,2,1};
+    for (int i = 0; i < 9; i++) {
+        printf("%4d", a[i] + b[i]);
+        if ((i + 1) % 3 == 0) printf("\n");
+    }
+}
+
+int main() {
+    dimensionaMatriz(3, 3);
+    int vet[tam];
+
+    zeraMatriz(vet);
+    imprimeMatriz(vet, linhas, colunas);
+
+    adicionaElemento(vet, 15, 1, 1);
+    adicionaElemento(vet, 25, 2, 2);
+    adicionaElemento(vet, 35, 3, 3);
+    imprimeMatriz(vet, linhas, colunas);
+
+    printf("O elemento que esta na linha 2 coluna 2 eh: %d\n\n", buscaElemento(vet, 2, 2));
+
+    printf("Soma Matrizes:\n");
+    somaMatriz();
+
+    return 0;
+}#include <stdio.h>
+
+int linhas, colunas, tam;
+
+void dimensionaMatriz(int l, int c) {
+    linhas = l;
+    colunas = c;
+    tam = l * c;
+}
+
+void zeraMatriz(int vet[]) {
+    for (int i = 0; i < tam; i++) vet[i] = 0;
+}
+
+int indice(int i, int j) {
+    return (i - 1) * colunas + (j - 1);
+}
+
+void adicionaElemento(int vet[], int valor, int i, int j) {
+    vet[indice(i, j)] = valor;
+}
+
+int buscaElemento(int vet[], int i, int j) {
+    return vet[indice(i, j)];
+}
+
+void imprimeMatriz(int vet[], int l, int c) {
+    for (int i = 1; i <= l; i++) {
+        for (int j = 1; j <= c; j++)
+            printf("%4d", vet[indice(i, j)]);
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void somaMatriz() {
+    int a[9] = {1,2,3,4,5,6,7,8,9};
+    int b[9] = {9,8,7,6,5,4,3,2,1};
+    for (int i = 0; i < 9; i++) {
+        printf("%4d", a[i] + b[i]);
+        if ((i + 1) % 3 == 0) printf("\n");
+    }
+}
+
+int main() {
+    dimensionaMatriz(3, 3);
+    int vet[tam];
+
+    zeraMatriz(vet);
+    imprimeMatriz(vet, linhas, colunas);
+
+    adicionaElemento(vet, 15, 1, 1);
+    adicionaElemento(vet, 25, 2, 2);
+    adicionaElemento(vet, 35, 3, 3);
+    imprimeMatriz(vet, linhas, colunas);
+
+    printf("O elemento que esta na linha 2 coluna 2 eh: %d\n\n", buscaElemento(vet, 2, 2));
+
+    printf("Soma Matrizes:\n");
+    somaMatriz();
+
+    return 0;
+}
